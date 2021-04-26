a14c374037885072bf4130209703351cdce48dbe
Andmontc <oscarandresmontes@gmail.com>
2021-04-26 11:23:32 -0500

    Task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module that returns a concatenated string """
+
+
+def concat(str1: str, str2: str) -> str:
+    """function that returns a concatenated string
+      str1: first string
+      str2: second string
+    Return: the concatenated string """
+    return str1 + str2
