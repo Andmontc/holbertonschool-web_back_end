124ab53618beabebbd7fc202d5e1459a0119e9e6
Andmontc <oscarandresmontes@gmail.com>
2021-04-27 16:03:17 -0500

    Task 2 Measure time in a function [fix pycodestyle]

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -5,12 +5,13 @@
 import asyncio
 from time import perf_counter
 
+
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """ function that measures the total execution time
         for the function wait_n """
     initTime = perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    return (perf_counter() - initTime) / n
+    return (perf_counter() - initTime)
