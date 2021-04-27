d988e4ffbeaa15a7c88c8a221771c2955eecbf90
Andmontc <oscarandresmontes@gmail.com>
2021-04-27 14:28:05 -0500

    Task 0 random number wait

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-""" return random number """
+""" Module return random number  """
 
 import asyncio
 from random import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """ Return random number """
+    """ Function that return a random number with asyncio """
     rand_number = random() * max_delay
     await asyncio.sleep(rand_number)
     return rand_number
