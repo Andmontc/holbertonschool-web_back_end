87cdabe281180be610d9764815c1941ac4dfb87f
Andmontc <oscarandresmontes@gmail.com>
2021-04-27 15:44:31 -0500

    Task 2 measure time in a function

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Module return random number  """
+
+
+import asyncio
+from time import perf_counter
+
+from asyncio.runners import run
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """ function that measures the total execution time for the function wait_n """
+    init = perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    return (perf_counter - init) / 2
