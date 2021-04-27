20dfe80381833ad14bf94c9c8d0a84af30981c89
Andmontc <oscarandresmontes@gmail.com>
2021-04-27 17:03:46 -0500

    Task 3 task creation [fix pycodestyle]

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,6 +6,7 @@ import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-def task_wait_random(max_delay: int) -> asyncio.Task :
-  """ Function that return a task """
-  return asyncio.create_task(wait_random(max_delay))
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """ Function that return a task """
+    return asyncio.create_task(wait_random(max_delay))
