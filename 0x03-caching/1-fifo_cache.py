79c84f001df208680fc6518a1136018d3f33f30c
Andmontc <oscarandresmontes@gmail.com>
2021-05-03 15:41:13 -0500

    Fifo caching [fix pycodestyle]

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -15,18 +15,20 @@ class FIFOCache(BaseCaching):
 
     def put(self, key, item):
         """ function that put items in dict """
-        
-        if len(self.cache_data) == self.MAX_ITEMS and key not in self.__datakeys:
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS and \
+                key not in self.__datakeys:
             discard = self.__datakeys.pop(0)
             del self.cache_data[discard]
             print('DISCARD: {}'.format(discard))
+
         if key and item:
             self.__datakeys.append(key)
             self.cache_data[key] = item
 
-            def get(self, key):
-                """ function that get the item from the dict """
-                if not key or key not in self.cache_data:
-                    return None
-                else:
-                    return self.cache_data[key]
+    def get(self, key):
+        """ function that get the item from the dict """
+        if not key or key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data[key]
