1d90d3f23c3ec8a6e7d019007002e072add8226c
Andmontc <oscarandresmontes@gmail.com>
2021-05-14 12:47:44 -0500

    update methods

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -29,7 +29,9 @@ class Auth():
 
     def authorization_header(self, request=None) -> str:
         """ auth header method """
-        return None
+        if request == None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ current user method """
