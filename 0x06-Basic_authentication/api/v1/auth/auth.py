41b4c11ddd491a7d53c74026fabc7af24ed93e87
Andmontc <oscarandresmontes@gmail.com>
2021-05-14 11:38:07 -0500

    auth class

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Module of auth
+"""
+
+from flask import request
+
+
+class Auth():
+    """ class auth """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require auth method """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ auth header method """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current user method """
+        return None
